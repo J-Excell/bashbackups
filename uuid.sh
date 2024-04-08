6ac8712182d2cc0f2347c913313c4ece429a883e
J-Excell <115702600+J-Excell@users.noreply.github.com>
2024-04-08 15:12:51 +0100

    Update uuid.sh
    added return value. not sure it's exactly what danny wants; I'll have to ask in class

--- uuid.sh
+++ uuid.sh
@@ -1,43 +1,25 @@
 # uuid
-echo UUID
-echo UUID 4
+echo UUID Generator
+echo "UUID 4:"
 
 first=$(dd if=/dev/random count=6 bs=1 2> /dev/null | xxd -ps)
 
 byte7=$(dd if=/dev/random count=1 bs=1 2> /dev/null | xxd -ps) 
-echo "Hexadecimal representation: ${byte7^^}"
-byte7=$(echo "ibase=16;obase=2;${byte7^^}" | bc -l) #convert to binary (works)
-echo "Binary representation: ${byte7}"
-
-byte7=$((2#$byte7 & 2#00001111))
-byte7=$(echo "ibase=10;obase=2;${byte7^^}" | bc -l) #convert to binary (works)
-echo "After and with 0x0f: $byte7"
-
-byte7=$((2#$byte7 | 2#01000000))
-byte7=$(echo "ibase=10;obase=2;${byte7^^}" | bc -l) #convert to binary (works)
-byte7=$(printf "%08d" "$byte7") #pad with zeros
-echo "After or with 0x40: $byte7"
-
-echo $byte7
-
-echo
+#echo "Original byte: ${byte7^^}"
+byte7=$((16#$byte7 & 10#15))
+byte7=$((10#$byte7 | 10#64))
+byte7=$(echo "ibase=10;obase=16;${byte7}" | bc -l) #convert dec back to hex
+#echo "After and with 0x0f and or with 0x40: $byte7"
 
 byte8=$(dd if=/dev/random count=1 bs=1 2> /dev/null | xxd -ps)
 
 byte9=$(dd if=/dev/random count=1 bs=1 2> /dev/null | xxd -ps) 
-echo "Hexadecimal representation: ${byte9^^}"
-byte9=$(echo "ibase=16;obase=2;${byte9^^}" | bc -l) #convert to binary (works)
-byte9=$(printf "%08d" "$byte9") #pad with zeros
-echo "Binary representation: ${byte9}"
-
-byte9=$((2#$byte9 & 2#00111111))
-byte9=$(echo "ibase=10;obase=2;${byte9^^}" | bc -l) #convert to binary (works)
-echo "After and with 0x3f: $byte9"
-byte9=$((2#$byte9 | 2#10000000))
-byte9=$(echo "ibase=10;obase=2;${byte9^^}" | bc -l) #convert to binary (works)
-echo "After or with 0x80: $byte9"
-echo $byte9
+#echo "Original byte: ${byte9^^}"
+byte9=$((16#$byte9 & 10#63))
+byte9=$((10#$byte9 | 10#128))
+byte9=$(echo "ibase=10;obase=16;${byte9}" | bc -l) #convert dec back to hex
+#echo "After and with 0x3f and or with 0x80: $byte9"
 
 last=$(dd if=/dev/random count=7 bs=1 2> /dev/null | xxd -ps)
 
-#result=$(( (first << 10) | (byte7 << 9) | (byte8 << 8) | (byte9 << 7) | last))
+echo "${first:0:8}-${first:8:4}-${byte7}${byte8}-${byte9}${last:0:2}-${last:2:12}"
