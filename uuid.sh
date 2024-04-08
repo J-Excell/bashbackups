6073d7fd16f905babf772ae7b1d85808538f79da
J-Excell <115702600+J-Excell@users.noreply.github.com>
2024-04-08 14:45:28 +0100

    Update uuid.sh
    i don't want to jinx it but i think this actually works

--- uuid.sh
+++ uuid.sh
@@ -1,20 +1,43 @@
 # uuid
+echo UUID
 echo UUID 4
 
-main=$(dd if=/dev/random count=14 bs=1 2> /dev/null | xxd -ps)
+first=$(dd if=/dev/random count=6 bs=1 2> /dev/null | xxd -ps)
 
-byte7=$(dd if=/dev/random count=1 bs=1 2> /dev/null | xxd -ps)
-printf "%x\n" $byte7 
-byte7=$((0x$byte7 & 0x0f))
-byte7=$((byte7 | 0x40))
-echo $($byte7 | xxd -ps)
+byte7=$(dd if=/dev/random count=1 bs=1 2> /dev/null | xxd -ps) 
+echo "Hexadecimal representation: ${byte7^^}"
+byte7=$(echo "ibase=16;obase=2;${byte7^^}" | bc -l) #convert to binary (works)
+echo "Binary representation: ${byte7}"
 
-byte9=$(dd if=/dev/random count=1 bs=1 2> /dev/null | xxd -ps)
-printf "%x\n" $byte9
-byte9=$((0x$byte9 & 0x3f))
-byte9=$((byte9 | 0x80))
-printf "%x\n" $byte9
+byte7=$((2#$byte7 & 2#00001111))
+byte7=$(echo "ibase=10;obase=2;${byte7^^}" | bc -l) #convert to binary (works)
+echo "After and with 0x0f: $byte7"
 
-result=${main:0:8}-${main:8:4}-${byte7}${main:12:2}-${byte9}${main:14:2}-${main:16:8}
+byte7=$((2#$byte7 | 2#01000000))
+byte7=$(echo "ibase=10;obase=2;${byte7^^}" | bc -l) #convert to binary (works)
+byte7=$(printf "%08d" "$byte7") #pad with zeros
+echo "After or with 0x40: $byte7"
 
-echo $result
+echo $byte7
+
+echo
+
+byte8=$(dd if=/dev/random count=1 bs=1 2> /dev/null | xxd -ps)
+
+byte9=$(dd if=/dev/random count=1 bs=1 2> /dev/null | xxd -ps) 
+echo "Hexadecimal representation: ${byte9^^}"
+byte9=$(echo "ibase=16;obase=2;${byte9^^}" | bc -l) #convert to binary (works)
+byte9=$(printf "%08d" "$byte9") #pad with zeros
+echo "Binary representation: ${byte9}"
+
+byte9=$((2#$byte9 & 2#00111111))
+byte9=$(echo "ibase=10;obase=2;${byte9^^}" | bc -l) #convert to binary (works)
+echo "After and with 0x3f: $byte9"
+byte9=$((2#$byte9 | 2#10000000))
+byte9=$(echo "ibase=10;obase=2;${byte9^^}" | bc -l) #convert to binary (works)
+echo "After or with 0x80: $byte9"
+echo $byte9
+
+last=$(dd if=/dev/random count=7 bs=1 2> /dev/null | xxd -ps)
+
+#result=$(( (first << 10) | (byte7 << 9) | (byte8 << 8) | (byte9 << 7) | last))
