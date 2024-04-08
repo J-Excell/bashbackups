aa5e3eb0a64ea7bf6d2b54e35986aa73661bc1c0
J-Excell <115702600+J-Excell@users.noreply.github.com>
2024-04-08 13:00:03 +0100

    Update uuid.sh
    no it doesn't work

--- uuid.sh
+++ uuid.sh
@@ -1,21 +1,20 @@
 # uuid
-echo UUID
 echo UUID 4
 
-first=$(dd if=/dev/random count=6 bs=1 2> /dev/null)
+main=$(dd if=/dev/random count=14 bs=1 2> /dev/null | xxd -ps)
 
-byte7=$(dd if=/dev/random count=1 bs=1 2> /dev/null) 
-echo " $((byte7))"
-byte7=$((0b$byte7 & 0b00001111))
-byte7=$((0b$byte7 | 0b01000000))
+byte7=$(dd if=/dev/random count=1 bs=1 2> /dev/null | xxd -ps)
+printf "%x\n" $byte7 
+byte7=$((0x$byte7 & 0x0f))
+byte7=$((byte7 | 0x40))
+echo $($byte7 | xxd -ps)
 
-byte8=$(dd if=/dev/random count=1 bs=1 2> /dev/null)
+byte9=$(dd if=/dev/random count=1 bs=1 2> /dev/null | xxd -ps)
+printf "%x\n" $byte9
+byte9=$((0x$byte9 & 0x3f))
+byte9=$((byte9 | 0x80))
+printf "%x\n" $byte9
 
-byte9=$(dd if=/dev/random count=1 bs=1 2> /dev/null) 
-byte9=$(($byte9 & 0b00111111))
-byte9=$(($byte9 | 0b10000000))
+result=${main:0:8}-${main:8:4}-${byte7}${main:12:2}-${byte9}${main:14:2}-${main:16:8}
 
-last=$(dd if=/dev/random count=7 bs=1 2> /dev/null)
-
-result=$(( (first << 2 ** 10) | (byte7 << 2 ** 9) | (byte8 << 2 ** 8) | \
-(byte9 << 2 ** 7) | last))
+echo $result
